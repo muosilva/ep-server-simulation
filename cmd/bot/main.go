0de7db87811c8328ada2c22804a2c4d9dcc4af1e
muriloliveira <murilolives20@gmail.com>
2024-10-20 12:23:11 -0300

    feature:adding-bot-requirement

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,1 +1,7 @@
 package main
+
+import "github.com/muosilva/ep-server-simulation/internal/bot"
+
+func main() {
+	bot.StartBot()
+}
--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -1,1 +1,43 @@
 package bot
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+func StartBot() {
+	conn, err := net.Dial("tcp", "localhost:3000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	done := make(chan struct{})
+
+	go func() {
+		scanner := bufio.NewScanner(conn)
+		for scanner.Scan() {
+			msg := scanner.Text()
+			fmt.Println("Mensagem de:", msg)
+
+			// Resposta invertida
+			reversed := reverseString(msg)
+			fmt.Fprintln(conn, reversed)
+		}
+		done <- struct{}{}
+	}()
+
+	// Bot faz login
+	fmt.Fprintln(conn, "\\changenick bot-inverter")
+	<-done
+}
+
+func reverseString(input string) string {
+	runes := []rune(input)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
